3e9a2b7f9066a068d3f78537188e5613c5aa3201
Sumit Vikram <vikramsumit@outlook.com>
2025-03-26 09:45:51 +0530

    first commit

--- python_code_cwh/27.py
+++ python_code_cwh/27.py
@@ -1,12 +1,12 @@
 # Exception Handling in python
 
-# a = input("Enter the number: ")
-# print(f"Multiplication table of {a} is: ")
-# try:
-#   for i in range(1, 11):
-#     print(f"{int(a)} X {i} = {int(a)*i}")
-# except:
-#   print("Invalid  Input!")
+a = input("Enter the number: ")
+print(f"Multiplication table of {a} is: ")
+try:
+  for i in range(1, 11):
+    print(f"{int(a)} X {i} = {int(a)*i}")
+except:
+  print("Invalid  Input!")
 
-# print("Some imp lines of code")
-# print("End of program")
+print("Some imp lines of code")
+print("End of program")
