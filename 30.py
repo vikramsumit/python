b109bbc7f6187103df1d079f08becce75560e8ed
Sumit Vikram <vikramsumit@outlook.com>
2025-03-30 18:32:26 +0530

    first commit

--- 30.py
+++ 30.py
@@ -1,5 +1,16 @@
 # Short Hand if, else statement
 
-a = 22222
-b = 2335
-print("A") if a > b else print("=") if a == b else print("B")
+# a = 22222
+# b = 2335
+# print("A") if a > b else print("=") if a == b else print("B")
+
+# Enumerate functions in python
+
+marks = [53,34,99,34,45,23,35,54,34,98,43,88]
+
+index = 0
+for mark in marks:
+    print(mark)
+    if(index == 2):
+        print("Awesome,boy")
+    index += 1
