750d746efc9fac38c9f4166713ab9a9c31f9da35
Sumit Vikram <vikramsumit@outlook.com>
2025-04-02 15:42:47 +0530

    first commit

---
+++ python_code_cwh/31.py
@@ -0,0 +1,17 @@
+# file handling in python
+
+
+# READING A FILE
+# f = open('myfile.txt', 'r')
+# # print(f)
+# text = f.read()
+# print(text)
+# f.close()
+
+# WRITING A FILE
+f = open("myfile2.txt", "w")
+f.write("Hello Raju bhai")
+f.close()
+
+with open("myfile2.txt", 'a') as f:
+    f.write("Hey i am available")
