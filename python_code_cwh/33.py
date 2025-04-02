682860883f39d439c1aa53a1a38a8753f451d2f7
Sumit Vikram <vikramsumit@outlook.com>
2025-04-02 16:14:54 +0530

    first commit

--- python_code_cwh/33.py
+++ python_code_cwh/33.py
@@ -1,7 +1,14 @@
-with open('myfile.txt','r') as f:
-    print(type(f))
-    f.seek(10)
-    print(f.tell())
-    data = f.read(11)
-    print(data)
-    f.close()
+# with open('myfile.txt','r') as f:
+#     print(type(f))
+#     f.seek(10)
+#     print(f.tell())
+#     data = f.read(11)
+#     print(data)
+#     f.close()
+
+with open('sample.txt', 'w') as f:
+  f.write('Hello World3!')
+  f.truncate(5)
+
+with open('sample.txt', 'r') as f:
+  print(f.read())
