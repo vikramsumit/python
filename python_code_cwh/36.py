f01e31a242dd8111c248249c3290eb44ccf1001c
Sumit Vikram <vikramsumit@outlook.com>
2025-04-08 23:44:58 +0530

    first commit

--- python_code_cwh/36.py
+++ python_code_cwh/36.py
@@ -16,8 +16,8 @@ sales3 = 6000
 profit3 = 2000
 ad3 = 1000
  
-RailwayForm   ---> Class [blueprint]
-harry --> harry ki info wala form --> Object [entity]
-tom --> tom ki info wala form --> Object [entity]
-shubham -- shubham ki info wala form --> Object [entity]
+# RailwayForm   ---> Class [blueprint]
+# harry --> harry ki info wala form --> Object [entity]
+# tom --> tom ki info wala form --> Object [entity]
+# shubham -- shubham ki info wala form --> Object [entity]
 # shubham.changeName("Shubhi")
