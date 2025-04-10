2ad326fb6d481f06a5217a62d503b90cbfceb8b1
Sumit Vikram <vikramsumit@outlook.com>
2025-04-10 23:45:17 +0530

    first commit

--- python_code_cwh/37.py
+++ python_code_cwh/37.py
@@ -18,3 +18,13 @@ a.info()
 b.name = "kaju"
 b.occupation = "Dry fruiit"
 b.info()
+
+class Details:
+    name = "Rohan"
+    age = 20
+
+    def desc(self):
+        print("My name is", self.name, "and I'm", self.age, "years old.")
+
+obj1 = Details()
+obj1.desc()
