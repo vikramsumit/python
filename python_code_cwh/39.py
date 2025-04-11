b90c8d38aafb6f351469b810df7ede9b1abfd2ed
Sumit Vikram <vikramsumit@outlook.com>
2025-04-11 12:41:33 +0530

    first commit

---
+++ python_code_cwh/39.py
@@ -0,0 +1,20 @@
+# Python decorators
+
+def greet(fx):
+    def mfx(*args, **kwargs):
+        print("Good Morning")
+        fx(*args, **kwargs)
+        print("Thanks for using this function")
+    return mfx
+    
+@greet
+def hello():
+    print("Hello World")
+    
+def add(a,b):
+    print(a+b)
+    
+# greet(hello)()
+
+hello()
+greet(add)(5,3)
