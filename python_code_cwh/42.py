64ee102fa6f2d98a5bca9eff3f555e543edb7260
Sumit Vikram <vikramsumit@outlook.com>
2025-04-12 12:00:09 +0530

    first commit

--- python_code_cwh/42.py
+++ python_code_cwh/42.py
@@ -1,34 +1,34 @@
-# Access Modifiers in python
+# # Access Modifiers in python
 
-class Employee:
-    def __init__(self):
-        self.__name = "Raju bhai"
-        self.address = "New york"
+# class Employee:
+#     def __init__(self):
+#         self.__name = "Raju bhai"
+#         self.address = "New york"
 
-a = Employee()
-# print(a.__name) #cannot access directly
-print(a._Employee__name) #can be accessed indirectly
-print(a.address)
-print(a.__dir__())
+# a = Employee()
+# # print(a.__name) #cannot access directly
+# print(a._Employee__name) #can be accessed indirectly
+# print(a.address)
+# print(a.__dir__())
 
 #protected methods
 class Student:
     def __init__(self):
-        self._name = "Harry"
+        self._name = "Raju"
 
     def _funName(self):      # protected method
-        return "CodeWithHarry"
+        return "CodeWithRaju"
 
 class Subject(Student):       #inherited class
-    pass
+    def __init__(self):
+        self._subject = "social study"
 
 obj = Student()
 obj1 = Subject()
 print(dir(obj))
 
-# calling by object of Student class
 print(obj._name)      
 print(obj._funName())     
 # calling by object of Subject class
-print(obj1._name)    
+# print(obj1._name)    
 print(obj1._funName())
