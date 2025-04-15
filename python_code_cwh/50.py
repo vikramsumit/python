bf58986b252063023b1c637e8b3072f088d3bed5
Sumit Vikram <vikramsumit@outlook.com>
2025-04-15 07:52:27 +0530

    first commit

--- python_code_cwh/50.py
+++ python_code_cwh/50.py
@@ -1,15 +1,31 @@
-class parentclass:
-    def parent_method(self):
-        print("This is parent class.")
+# class parentclass:
+#     def parent_method(self):
+#         print("This is parent class.")
 
-class childclass(parentclass):
-    def child_method(self):
-        print("Raju")
-        super().parent_method()
-        print("This is child class.")
-        super().parent_method()
+# class childclass(parentclass):
+#     def child_method(self):
+#         print("Raju")
+#         super().parent_method()
+#         print("This is child class.")
+#         super().parent_method()
 
-child_object = childclass()
-child_object.child_method()
-child_object.parent_method()
+# child_object = childclass()
+# child_object.child_method()
+# child_object.parent_method()
 
+class Employee:
+    def __init__(self, name, id):
+        self.name = name
+        self.id = id
+
+class Programmer(Employee):
+    def __init__(self, name, id, expertise):
+        # self.name = name
+        # self.id = id
+        super().__init__(name,id)
+        self.expertise = expertise
+        
+ram = Employee("Ram", "94387")
+shyam = Programmer("Shyam", "3947", "PYTHON")
+print(shyam.expertise)
+print(shyam.id)
