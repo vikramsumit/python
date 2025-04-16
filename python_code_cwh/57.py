a9da0c7ed68966d354d7a77bf37dcdd430137dbc
Sumit Vikram <vikramsumit@outlook.com>
2025-04-16 19:45:40 +0530

    first commit

---
+++ python_code_cwh/57.py
@@ -0,0 +1,35 @@
+# Multilevel Inheritence
+
+class Animal:
+    def __init__(self, name, species):
+        self.name = name
+        self.species = species
+        
+    def show_details(self):
+        print(f"Name: {self.name}")
+        print(f"Species: {self.species}")
+        
+class Dog(Animal):
+    def __init__(self, name, breed):
+        Animal.__init__(self, name, species="Dog")
+        self.breed = breed
+        
+    def show_details(self):
+        Animal.show_details(self)
+        print(f"Breed: {self.breed}")
+        
+class GoldenRetriever(Dog):
+    def __init__(self, name, color):
+        Dog.__init__(self, name, breed="German Shepherd")
+        self.color = color
+        
+    def show_details(self):
+        Dog.show_details(self)
+        print(f"Color: {self.color}")
+
+o = Dog("tommy", "Black")
+o.show_details()
+p = GoldenRetriever("kalia", "Brown")
+p.show_details()
+print(GoldenRetriever.mro())
+
