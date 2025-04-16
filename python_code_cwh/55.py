54ae08babd6da4d8199201a1e1a85820c9325360
Sumit Vikram <vikramsumit@outlook.com>
2025-04-16 19:17:15 +0530

    first commit

---
+++ python_code_cwh/55.py
@@ -0,0 +1,34 @@
+class Animal:
+    def __init__(self, name, species):
+        self.name = name
+        self.species = species
+        
+    def make_sound(self):
+        print("Sound made by the animal")
+
+class Dog(Animal):
+    def __init__(self, name, breed):
+        Animal.__init__(self, name, species="Dog")
+        self.breed = breed
+        
+    def make_sound(self):
+        print("Bark!")
+        
+class Cat(Animal):
+    def __init__(self,name,breed):
+        Animal.__init__(self, name, species="Cat")
+        self.breed = breed
+
+    def make_sound(self):
+        print("Meow Meow")
+
+d = Dog("Dog", "Doggerman")
+d.make_sound()
+
+a = Animal("Dog", "Dog")
+a.make_sound()
+
+c = Cat("Cat", "tiger species")
+c.make_sound()
+
+# Quick Quiz: Implement a Cat class by using the animal class. Add some methods specific to cat
