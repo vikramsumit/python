a021ee0885b782bfc42e119d78c782f6c242b7b8
Sumit Vikram <vikramsumit@outlook.com>
2025-04-19 22:28:51 +0530

    first commit

--- python_code_cwh/59.py
+++ python_code_cwh/59.py
@@ -1,21 +1,21 @@
 import time 
 
-# def usingWhile():
-#   i = 0
-#   while i<50000:
-#     i = i +1
-#     print(i) 
+def usingWhile():
+  i = 0
+  while i<50000:
+    i = i +1
+    print(i) 
 
-# def usingFor():
-#   for i in range(50000):
-#     print(i)
+def usingFor():
+  for i in range(50000):
+    print(i)
 
-# init = time.time()
-# usingFor()
+init = time.time()
+usingFor()
 # t1 = time.time() - init
 # init = time.time()
 # usingWhile()
-# print(time.time() - init)
+print(time.time() - init)
 # print(t1)
 
 
