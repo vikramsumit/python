f3677e34ca328bdc0b7d0d5c6bd016c3d56e1529
Sumit Vikram <vikramsumit@outlook.com>
2025-04-21 12:25:20 +0530

    first commit

--- python_code_cwh/61.py
+++ python_code_cwh/61.py
@@ -1,4 +1,27 @@
-numbers = [1, 2, 3, 4, 5]
+# walrus operator :=
 
-while (n := len(numbers)) > 0:
-    print(numbers.pop())
+# new to Python 3.8
+# assignment expression aka walrus operator
+# assigns values to variables as part of a larger expression
+
+# numbers = [1, 2, 3, 4, 5]
+
+# while (n := len(numbers)) > 0:
+#     print(numbers.pop())
+
+
+# happy = False
+# print(happy)
+
+# print(happy := True)
+
+# foods = list()
+# while True:
+#   food = input("What food do you like?: ")
+#   if food == "quit":
+#       break
+#   foods.append(food)
+
+foods = list()
+while (food := input("What food do you like?: ")) != "quit":
+    foods.append(food)
