4f2ae08588197f56813237a12d0154d084395cea
Sumit Vikram <vikramsumit@outlook.com>
2025-04-22 23:44:29 +0530

    first commit

--- python_code_cwh/68.py
+++ python_code_cwh/68.py
@@ -9,7 +9,7 @@ text = '''Cyclone Dumazile was a strong tropical cyclone in the South-West India
 match = re.search(pattern, text)
 print(match)
 
-matches = re.finditer(pattern, text)
-for match in matches:
-  print(match.span()) 
-  print(text[match.span()[0]: match.span()[1]])
+# matches = re.finditer(pattern, text)
+# for match in matches:
+#   print(match.span()) 
+#   print(text[match.span()[0]: match.span()[1]])
