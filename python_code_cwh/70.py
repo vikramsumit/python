cf8fc5986efd365979bc82520da1fc4d47bbd660
Sumit Vikram <vikramsumit@outlook.com>
2025-04-26 01:18:06 +0530

    first commit

--- python_code_cwh/70.py
+++ python_code_cwh/70.py
@@ -1,35 +1,35 @@
-import threading
-import time
+# import threading
+# import time
 # from concurrent.futures import ThreadPoolExecutor
 
-# Indicates some task being done
-def func(seconds):
-  print(f"Sleeping for {seconds} seconds")
-  time.sleep(seconds)
+# # Indicates some task being done
+# def func(seconds):
+#   print(f"Sleeping for {seconds} seconds")
+#   time.sleep(seconds)
 #   return seconds
 
 # def main():
-#   time1 = time.perf_counter()
-  # Normal Code
-# func(4) 
-# func(2)
-# func(1)
+# time1 = time.perf_counter()
+#   # Normal Code
+# # func(4) 
+# # func(2)
+# # func(1)
   
   
-  # Same code using Threads
-t1 = threading.Thread(target=func, args=[4])
-t2 = threading.Thread(target=func, args=[2])
-t3 = threading.Thread(target=func, args=[1])
-t1.start()
-t2.start()
-t3.start()
+#   # Same code using Threads
+# t1 = threading.Thread(target=func, args=[4])
+# t2 = threading.Thread(target=func, args=[2])
+# t3 = threading.Thread(target=func, args=[1])
+# t1.start()
+# t2.start()
+# t3.start()
   
-#   t1.join()
-#   t2.join()
-#   t3.join()
-#   # Calculating Time 
-#   time2 = time.perf_counter()
-#   print(time2 - time1)
+# t1.join()
+# t2.join()
+# t3.join()
+# #   # Calculating Time 
+# time2 = time.perf_counter()
+# print(time2 - time1)
 
 
 # def poolingDemo():
@@ -47,3 +47,49 @@ t3.start()
 
 
 # poolingDemo()
+
+import threading
+import time
+from concurrent.futures import ThreadPoolExecutor
+
+# Task function
+def func(seconds):
+    print(f"Sleeping for {seconds} seconds")
+    time.sleep(seconds)
+    return seconds
+
+def main():
+    time1 = time.perf_counter()
+    
+    # Using threads
+    t1 = threading.Thread(target=func, args=[4])
+    t2 = threading.Thread(target=func, args=[2])
+    t3 = threading.Thread(target=func, args=[1])
+
+    t1.start()
+    t2.start()
+    t3.start()
+
+    t1.join()
+    t2.join()
+    t3.join()
+    
+    time2 = time.perf_counter()
+    print(f"Time taken using threading: {time2 - time1:.2f} seconds")
+
+def poolingDemo():
+    with ThreadPoolExecutor() as executor:
+        future1 = executor.submit(func, 3)
+        future2 = executor.submit(func, 2)
+        future3 = executor.submit(func, 4)
+        print(future1.result())
+        print(future2.result())
+        print(future3.result())
+        # l = [3, 5, 1, 2]
+        # results = executor.map(func, l)
+        # for result in results:
+        #     print(result)
+
+# Call the functions
+main()
+poolingDemo()
