a6d770f66c40721ad257124d5b3fe6591f8af9db
sumit vikram <vikramsumit@outlook.com>
2025-05-30 23:24:01 +0530

    first commit

--- python_code_cwh/45.py
+++ python_code_cwh/45.py
@@ -8,11 +8,11 @@ class Employee:
     def showdetails(self):
         print(f"The name of the employee is {self.name} and the raise amount in {self.companyname} is {self.raise_amount}.\n Total no of employee in company is{self.noofEmployees}")
 
-# emp1 = Employee("Rajubhai")
-# emp1.raise_amount = 0.7
-# Employee.companyname = "Google"
-# emp1.companyname = "AppleIndia"
-# emp1.showdetails()
+emp1 = Employee("Rajubhai")
+emp1.raise_amount = 0.7
+Employee.companyname = "Google"
+emp1.companyname = "AppleIndia"
+emp1.showdetails()
 # Employee.showdetails(emp1)
 
 emp2 = Employee("Babubahi")
