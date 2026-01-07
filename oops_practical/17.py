51c81477c7038194f3c7e8f469ba20794d1ebfa8
sumit vikram <vikramsumit@outlook.com>
2026-01-07 23:50:48 +0530

    Enhancement

---
+++ oops_practical/17.py
@@ -0,0 +1,18 @@
+# Circle Class: Create a Circle class with an attribute for the radius. Add methods to calculate the area and circumference of the circle.
+import math
+
+class Circle:
+    def __init__(self,radius):
+        self.radius = radius
+    
+    def area(self):
+        return math.pi* self.radius ** 2
+    
+    def circumference(self):
+        return 2 * math.pi * self.radius
+    
+input = print(float(input("Enter the radius of circle: ")))
+
+circle = Circle(input)
+
+print(f"Area: {circle.area():.2f}")
