84611859131c4fb53b40afb9f592cc66f958f312
sumit vikram <vikramsumit@outlook.com>
2026-01-09 01:01:35 +0530

    Enhancement

--- oops_practical/23.py
+++ oops_practical/23.py
@@ -1,1 +1,12 @@
 # Product Constructor: Create a Product class with a constructor that initializes attributes for product name and price. Instantiate different products with varying details.
+
+class Product:
+    def __init__(self, product, name, price):
+        self.product = product
+        self.name = name
+        self.price = price
+
+product1 = Product("smartphone", "samsung s24", 55000)
+
+print(f"{product1.product} - {product1.name} - {product1.price}")
+        
