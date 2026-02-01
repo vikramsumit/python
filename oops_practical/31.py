7b91546d22f65df04cd3ad75dea0e5146d2d9ab1
sumit vikram <vikramsumit@outlook.com>
2026-02-01 23:34:32 +0530

    os practical

--- oops_practical/31.py
+++ oops_practical/31.py
@@ -1,6 +1,6 @@
 # Online Shopping Cart: Create an online shopping cart system using inheritance. Design a base class "Product" and derived classes for various product categories (e.g., "Electronics," "Clothing," "Books"). Implement methods for adding/removing items from the cart and calculating the total price with appropriate discounts for each category.
 
-# class Product:
+class Product:
     def __init__(self, name, price):
         self.name = name
         self.price = price
